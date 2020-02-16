886fc7dcc4d57bd8537a0bc6a1b413bcfc81e952
Adam Antinoo <adamantinoo.git@gmail.com>
2020-02-17 00:51:01 +0100

    -[FRONTEND] Create the domain and dto classes for the new Fitting and FittingItem instances to be obtained from the backend.
    Former-commit-id: d06f482ba7e1531caac5ef604378e4c37e4b7263

--- NeoCom.Infinity.Frontend/src/app/domain/Fitting.domain.spec.ts
+++ NeoCom.Infinity.Frontend/src/app/domain/Fitting.domain.spec.ts
@@ -1,7 +1,43 @@
+// - TESTING
+import { TestBed } from '@angular/core/testing';
+// - PROVIDERS
+import { SupportIsolationService } from '@app/testing/SupportIsolation.service';
+// - DOMAIN
 import { Fitting } from './Fitting.domain';
+import { FittingItem } from './FittingItem.domain';
 
-describe('Fitting', () => {
-    it('should create an instance', () => {
-        expect(new Fitting()).toBeTruthy();
+fdescribe('CLASS Fitting [Module: DOMAIN]', () => {
+    let isolation: SupportIsolationService;
+
+    beforeEach(() => {
+        isolation = TestBed.get(SupportIsolationService);
+    });
+    // - C O N S T R U C T I O N   P H A S E
+    fdescribe('Construction Phase', () => {
+        it('Should be created', () => {
+            console.log('><[domain/Fitting]> should be created');
+            expect(new Fitting()).toBeTruthy();
+        });
+        it('Should perform the constructor', () => {
+            console.log('><[domain/Fitting]> Should perform the constructor');
+            let fittingItem = new FittingItem({
+                typeId: 123,
+                name: '-TEST-NAME-',
+                location: '-TEST-LOCATION-'
+            })
+            expect(new Fitting({
+                items: [fittingItem]
+            })).toBeTruthy();
+        });
+    });
+    // - C O D E   C O V E R A G E   P H A S E
+    xdescribe('Code Coverage Phase [getters]', () => {
+        it('getName.success: check the name field', () => {
+            const expectedName = isolation.generateRandomString(32);
+            const expected = new Object({ name: expectedName });
+            // const corporation = new Corporation({ corporationPublicData: expected })
+            // const obtained = corporation.getName();
+            // expect(obtained).toBe(expectedName);
+        });
     });
 });
--- NeoCom.Infinity.Frontend/src/app/domain/FittingItem.domain.spec.ts
+++ NeoCom.Infinity.Frontend/src/app/domain/FittingItem.domain.spec.ts
@@ -1,7 +1,31 @@
+// - TESTING
+import { TestBed } from '@angular/core/testing';
+// - PROVIDERS
+import { SupportIsolationService } from '@app/testing/SupportIsolation.service';
+// - DOMAIN
 import { FittingItem } from './FittingItem.domain';
 
-describe('FittingItem', () => {
-  it('should create an instance', () => {
-    expect(new FittingItem()).toBeTruthy();
-  });
+fdescribe('CLASS FittingItem [Module: DOMAIN]', () => {
+    let isolation: SupportIsolationService;
+
+    beforeEach(() => {
+        isolation = TestBed.get(SupportIsolationService);
+    });
+    // - C O N S T R U C T I O N   P H A S E
+    fdescribe('Construction Phase', () => {
+        it('Should be created', () => {
+            console.log('><[domain/Fitting]> should be created');
+            expect(new FittingItem()).toBeTruthy();
+        });
+    });
+    // - C O D E   C O V E R A G E   P H A S E
+    xdescribe('Code Coverage Phase [getters]', () => {
+        it('getName.success: check the name field', () => {
+            const expectedName = isolation.generateRandomString(32);
+            const expected = new Object({ name: expectedName });
+            // const corporation = new Corporation({ corporationPublicData: expected })
+            // const obtained = corporation.getName();
+            // expect(obtained).toBe(expectedName);
+        });
+    });
 });
