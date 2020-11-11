1cec993b9ed734cf1733e590b76adeb33097d7ec
Adam Antinoo <adamantinoo.git@gmail.com>
2020-11-11 12:24:22 +0100

    -[REFACTOR] Create construction factories for GetCharacter esi clasess to work classes that should need ESI Data access for completion.
    Former-commit-id: fc0121a3b16bfa2b9e7879d7887e72d307661f8c

--- NeoCom-Infinity-Backend/src/main/java/org/dimensinfin/eveonline/neocom/infinity/backend/industry/fitting/serializer/FittingItemSerializer.java
+++ NeoCom-Infinity-Backend/src/main/java/org/dimensinfin/eveonline/neocom/infinity/backend/industry/fitting/serializer/FittingItemSerializer.java
@@ -1,26 +1,29 @@
-package org.dimensinfin.eveonline.neocom.infinity.core.serializer;
+package org.dimensinfin.eveonline.neocom.infinity.backend.industry.fitting.serializer;
 
 import java.io.IOException;
 
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 
 import org.dimensinfin.eveonline.neocom.domain.FittingItem;
+import org.dimensinfin.eveonline.neocom.infinity.backend.universe.item.rest.v2.EsiItemControllerV2;
 
 @JsonComponent
 public class FittingItemSerializer extends JsonSerializer<FittingItem> {
 	@Override
 	public void serialize( final FittingItem value, final JsonGenerator jgen, final SerializerProvider provider )
-			throws IOException, JsonProcessingException {
+			throws IOException {
 		jgen.writeStartObject();
 
-		jgen.writeStringField( "jsonClass", value.getJsonClass() );
-		jgen.writeNumberField( "neoItemId", value.getTypeId() );
+		jgen.writeNumberField( "typeId", value.getTypeId() );
 		jgen.writeStringField( "name", value.getTypeName() );
-		jgen.writeStringField( "location", value.getFlag().toString() );
+		jgen.writeStringField( "location", value.getFlag().toString().toUpperCase() );
+		jgen.writeObjectField( "item", WebMvcLinkBuilder.linkTo(
+				WebMvcLinkBuilder.methodOn( EsiItemControllerV2.class ).getItem( value.getTypeId() )
+		).withRel( "item" ) );
 
 		jgen.writeEndObject();
 	}
