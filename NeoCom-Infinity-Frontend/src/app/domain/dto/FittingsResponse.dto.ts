a8a95e6020a4b883e78438ece61e2dda7ee00f17
Adam Antinoo <adamantinoo.git@gmail.com>
2020-11-15 13:21:25 +0100

    -[FRONTEND] Adding some more unit test code.
    Former-commit-id: 94ab2897b4fa9465932fb293b22219c08c0b7347

--- NeoCom-Infinity-Frontend/src/app/domain/dto/FittingsResponse.dto.ts
+++ NeoCom-Infinity-Frontend/src/app/domain/dto/FittingsResponse.dto.ts
@@ -2,7 +2,7 @@
 import { NeoComResponse } from './NeoComResponse.dto';
 import { Fitting } from '../Fitting.domain';
 
-export class ServerInfoResponse extends NeoComResponse {
+export class ServerInfoResponse_depre extends NeoComResponse {
     public fittings: Fitting[];
 
     constructor(values: Object = {}) {
--- NeoCom-Infinity-Frontend/src/app/domain/hal/HALLink.hal.ts
+++ NeoCom-Infinity-Frontend/src/app/domain/hal/HALLink.hal.ts
@@ -4,33 +4,46 @@ import { Observable } from 'rxjs'
 import { HALResolver } from '@app/services/HALResolver.service'
 
 export class HALLink<T> {
-    public isDownloaded: boolean = false
-    public rel: string
-    public href: string
+    private downloaded: boolean = false
+    private rel: string
+    private href: string
     public target: T
 
     constructor(values: Object = {}) {
         Object.assign(this, values)
     }
 
-    public access(resolver: HALResolver): Promise<T | any> {
-        if (this.isDownloaded)
-            return new Observable(subscriber => {
+    public access(resolver: HALResolver): Promise<T> {
+        if (this.isDownloaded())
+            return new Observable<T>(subscriber => {
                 subscriber.next(this.target)
                 subscriber.complete()
             }).toPromise()
         else {
-            return new Observable(subscriber => {
+            return new Observable<T>(subscriber => {
                 if (null != resolver)
                     resolver.resolve(this.href)
                         .subscribe((entrydata: any): void => {
                             this.target = entrydata
-                            this.isDownloaded = true
+                            this.downloaded = true
                             subscriber.next(this.target)
                             subscriber.complete();
                         })
                 else subscriber.next(this.target)
             }).toPromise()
         }
+    }
+    // - G E T T E R S   &   S E T T E R S
+    public isDownloaded(): boolean {
+        return this.downloaded
+    }
+    public getRelation(): string {
+        return this.rel
+    }
+    public getHref(): string {
+        return this.href
+    }
+    public getTarget(): T {
+        return this.target
     }
 }
