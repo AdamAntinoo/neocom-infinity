329804f465595c2e25f998ac8bb83ae0dea35356
Adam Antinoo W10 <adamantinoo.git@gmail.com>
2020-11-17 15:34:45 +0100

    -[FRONTEND] New project to fast process planetary interaction data. To be merged on NeoCom project.
    Former-commit-id: 2c91a791a3e972821e2aa9d53402636bf9a3d5e6

---
+++ neocom-planetary-interaction/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'npi-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'neocom-planetary-interaction';
+}
---
+++ neocom-planetary-interaction/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  dataLocation : '../assets/data/'
+}
+import 'zone.js/dist/zone-error';  // Included with Angular CLI.
