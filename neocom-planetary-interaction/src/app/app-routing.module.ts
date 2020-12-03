2090384047f608bd0cc83957ddc7a51886342a6f
Adam Antinoo <adamantinoo.git@gmail.com>
2020-12-03 23:35:13 +0100

    -[PLANETARY] Added a dnd library but not operative. Returning to know library.
    Former-commit-id: d763fa2443a1b95ad7780adb7f541e81d733bcf4

--- neocom-planetary-interaction/src/app/app-routing.module.ts
+++ neocom-planetary-interaction/src/app/app-routing.module.ts
@@ -2,23 +2,23 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: '/planetary/analysis',
-    pathMatch: 'full',
-  },
-  // - LOGIN PAGES
-  {
-    path: 'planetary',
-    loadChildren: () =>
-      import('./modules/planetary/planetary.module').then(
-        (m) => m.PlanetaryModule
-      ),
-  },
+    {
+        path: '',
+        redirectTo: '/planetary/analysis',
+        pathMatch: 'full',
+    },
+    // - LOGIN PAGES
+    {
+        path: 'planetary',
+        loadChildren: () =>
+            import('./modules/planetary/planetary.module').then(
+                (m) => m.PlanetaryModule
+            ),
+    },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
