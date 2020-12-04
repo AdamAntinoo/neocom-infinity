830f0bc7776a600ec94296c9e1919806478934b2
Adam Antinoo W10 <adamantinoo.git@gmail.com>
2020-12-04 16:35:45 +0100

    -[PLANETARY] Updated the interconnection between the different panels using an intermediate data service.
    Former-commit-id: 3cdff4ce94a79dd5aa17ed293b359825c46dd328

--- neocom-planetary-interaction/src/app/services/planetary-data.service.ts
+++ neocom-planetary-interaction/src/app/services/planetary-data.service.ts
@@ -12,6 +12,8 @@ import { environment } from 'src/environments/environment';
 })
 export class PlanetaryDataService {
     private planetIconTranslationTable: Map<string, string> = new Map<string, string>()
+    private selectedPlanets: PlanetaryDataRecord[] = []
+    private selectedResources: GeneratedResource[] = []
 
     constructor() {
         // Initialize planetary data conversion structures.
@@ -19,11 +21,33 @@ export class PlanetaryDataService {
         this.planetIconTranslationTable.set('gas', 'planet_gas_103_128_3.png')
         this.planetIconTranslationTable.set('ice', 'planet_ice_103_128_1.png')
         this.planetIconTranslationTable.set('lava', 'planet_lava_102_128_1.png')
+        this.planetIconTranslationTable.set('oceanic', 'planet_oceanic_104_128_2.png')
+        this.planetIconTranslationTable.set('plasma', 'planet_plasma_103_128_2.png')
+        this.planetIconTranslationTable.set('storm', 'planet_storm_102_128_2.png')
+        this.planetIconTranslationTable.set('temperate', 'planet_temperate_102_128_4.png')
     }
 
     public getPlanetIconByType(type: string): string {
         const hit = this.planetIconTranslationTable.get(type)
         if (null != hit) return hit
-        else return 'planet_barren_102_128_3.png'
+        else return 'defaulticonplaceholder.png'
+    }
+    public clean(): void {
+        this.selectedPlanets = []
+        this.selectedResources = []
+    }
+    public updateSelectedPlanets(newSelectedPlanetList: PlanetaryDataRecord[]): void {
+        console.log('V1PlanetaryPageComponent.updatePlanetList')
+        this.selectedPlanets = newSelectedPlanetList
+    }
+    public updateResourceList(newSelectedResourceList: GeneratedResource[]): void {
+        console.log('V1PlanetaryPageComponent.updateResourceList')
+        this.selectedResources = newSelectedResourceList
+    }
+    public getSelectedPlanets(): PlanetaryDataRecord[] {
+        return this.selectedPlanets
+    }
+    public getSelectedResources(): GeneratedResource[] {
+        return this.selectedResources
     }
 }
