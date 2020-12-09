164a1bb0f4e90b0d0417361aa5630406564940f6
Adam Antinoo <adamantinoo.git@gmail.com>
2020-12-09 15:26:10 +0100

    -[CI] Setting the new configuration to deploy the development version for the Planetary feature.
    Former-commit-id: 362ce40b3e177ac0ca6af9dbfaccd9ec43f5f047

--- NeoCom-Infinity-Frontend/src/app/modules/renders/v1-planet-data-render/v1-planet-data-render.component.ts
+++ NeoCom-Infinity-Frontend/src/app/modules/renders/v1-planet-data-render/v1-planet-data-render.component.ts
@@ -1,15 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { NodeContainerRenderComponent } from '@shared/renders/node-container-render/node-container-render.component';
 
 @Component({
-  selector: 'v1-planet-data',
-  templateUrl: './v1-planet-data-render.component.html',
-  styleUrls: ['./v1-planet-data-render.component.scss']
+    selector: 'v1-planet-data',
+    templateUrl: './v1-planet-data-render.component.html',
+    styleUrls: ['./v1-planet-data-render.component.scss']
 })
-export class V1PlanetDataRenderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class V1PlanetDataRenderComponent extends NodeContainerRenderComponent {
 
 }
