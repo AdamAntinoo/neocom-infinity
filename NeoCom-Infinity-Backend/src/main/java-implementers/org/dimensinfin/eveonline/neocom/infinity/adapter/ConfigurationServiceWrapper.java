259254613ebbdcd6743fde2520d4261d72a3f51a
Adam Antinoo <adamantinoo.git@gmail.com>
2021-01-03 13:32:31 +0100

    -[DATAMANAGEMENT][BUG] Configuration Service initialization should read the properties. There could be more than a single instance and property read action should not be delegated.
    Former-commit-id: 9bb183dc9606f9406085aca0295b19eb98850f40

--- NeoCom-Infinity-Backend/src/main/java-implementers/org/dimensinfin/eveonline/neocom/infinity/adapter/ConfigurationServiceWrapper.java
+++ NeoCom-Infinity-Backend/src/main/java-implementers/org/dimensinfin/eveonline/neocom/infinity/adapter/ConfigurationServiceWrapper.java
@@ -1,41 +1,40 @@
 package org.dimensinfin.eveonline.neocom.infinity.adapter;
 
-import java.io.IOException;
-import java.text.MessageFormat;
 import java.util.Objects;
-import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import org.dimensinfin.eveonline.neocom.infinity.adapter.implementers.SBConfigurationService;
 import org.dimensinfin.eveonline.neocom.provider.IConfigurationService;
-import org.dimensinfin.logging.LogWrapper;
 
 @Component
 public class ConfigurationServiceWrapper {
 	private static final String DEFAULT_PROPERTIES_DIRECTORY = "properties";
 	private String propertiesDirectory;
-	private SBConfigurationService singleton;
+	private IConfigurationService singleton;
 
+	// - C O N S T R U C T O R S
+	//	public ConfigurationServiceWrapper( @Value("${P.runtime.configuration.properties.path}") final String configuredLocation ) throws IOException {
+	//		// Set the final configuration path from the properties or from default.
+	//		if (null == configuredLocation) this.propertiesDirectory = DEFAULT_PROPERTIES_DIRECTORY;
+	//		else this.propertiesDirectory = configuredLocation;
+	//	}
 	@Autowired
-	public ConfigurationServiceWrapper( @Value("${P.runtime.configuration.properties.path}") final String configuredLocation ) throws IOException {
-		// Set the final configuration path from the properties or from default.
-		if (null == configuredLocation) this.propertiesDirectory = DEFAULT_PROPERTIES_DIRECTORY;
-		else this.propertiesDirectory = configuredLocation;
+	public ConfigurationServiceWrapper( final IConfigurationService configurationService ) {
+		this.singleton = configurationService;
 	}
 
+	// - G E T T E R S   &   S E T T E R S
 	public IConfigurationService getSingleton() {
 		return Objects.requireNonNull( this.singleton );
 	}
 
-	@PostConstruct
-	void build() throws IOException {
-		LogWrapper.info( MessageFormat.format( "Configured Properties location: {0}",this.propertiesDirectory ) );
-		this.singleton = new SBConfigurationService.Builder()
-				.optionalPropertiesDirectory( this.propertiesDirectory )
-				.build();
-//		this.singleton.readAllProperties();
-	}
+	//	@PostConstruct
+	//	void build() throws IOException {
+	//		LogWrapper.info( MessageFormat.format( "Configured Properties location: {0}",this.propertiesDirectory ) );
+	//		this.singleton = new SBConfigurationService.Builder()
+	//				.optionalPropertiesDirectory( this.propertiesDirectory )
+	//				.build();
+	////		this.singleton.readAllProperties();
+	//	}
 }
