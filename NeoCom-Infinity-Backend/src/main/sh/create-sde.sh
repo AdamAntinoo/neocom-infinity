c280bd84cd4c5a13ab6ef7be40e8cab4b7d7cf4a
Adam Antinoo <adamantinoo.git@gmail.com>
2021-01-05 19:58:44 +0100

    -[PROJECT] Update the SDE database to add the new table and scripts to create the database.
    Former-commit-id: 9ecf680081e6777f4ec152e976c9ea4050af4472

--- NeoCom-Infinity-Backend/src/main/sh/create-sde.sh
+++ NeoCom-Infinity-Backend/src/main/sh/create-sde.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 figlet CreateSDE
 echo "> Database creation started."
+mv ./downloads/*.csv .
 
 echo ">> Remove current sde database..."
 rm sde.db
@@ -8,3 +9,4 @@ echo ">> Create new sde database..."
 /usr/bin/sqlite3 sde.db < db-ddl-0.20.0.ddl
 cp sde.db ../resources/
 echo "> Database creation completed."
+rm -rf *.csv
--- NeoCom-Infinity-Backend/src/main/sh/update-sde.sh
+++ NeoCom-Infinity-Backend/src/main/sh/update-sde.sh
@@ -4,7 +4,7 @@ WORKING_DIRECTORY=$(pwd)
 downloadFiles() {
   cd "${WORKING_DIRECTORY}/downloads" || exit 1
   allFiles=("mapRegions" "mapConstellations" "mapSolarSystems" "staStationTypes" "planetSchematicsPinMap" "planetSchematicsTypeMap" "invTypes"
-  "industryActivityMaterials" "industryActivitySkills")
+  "industryActivityMaterials" "industryActivitySkills" "industryActivityProducts")
 
   for file in ${allFiles[@]}; do
     curl -L -o "$file.csv.bz2" "https://www.fuzzwork.co.uk/dump/latest/$file.csv.bz2"
@@ -13,7 +13,7 @@ downloadFiles() {
 decompress() {
   cd "${WORKING_DIRECTORY}/downloads" || exit 1
   bzip2 -d *.bz2
-  mv *.csv ../
+#  mv *.csv ../
 }
 
 figlet UpdateSDE
