c4624b563e70c566c0d08eaa4c20715ac6b5f697
Adam Antinoo <adamantinoo.git@gmail.com>
2021-01-11 15:41:22 +0100

    -[TESTING] Adding more unit test code for sections that are in use but have not test code.
    Former-commit-id: 3e220461440b1854354a9a6ffec95199babcd247

--- NeoCom-Infinity-Backend/src/main/java/org/dimensinfin/eveonline/neocom/infinity/backend/scheduler/ScheduleBlueprintProcessor.java
+++ NeoCom-Infinity-Backend/src/main/java/org/dimensinfin/eveonline/neocom/infinity/backend/scheduler/ScheduleBlueprintProcessor.java
@@ -2,39 +2,36 @@ package org.dimensinfin.eveonline.neocom.infinity.backend.scheduler;
 
 import javax.validation.constraints.NotNull;
 
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.stereotype.Component;
-
-import org.dimensinfin.annotation.LogEnterExit;
 import org.dimensinfin.eveonline.neocom.infinity.backend.scheduler.jobs.BlueprintProcessorJob;
 import org.dimensinfin.eveonline.neocom.provider.IConfigurationService;
 import org.dimensinfin.eveonline.neocom.service.ESIDataService;
 import org.dimensinfin.eveonline.neocom.service.scheduler.JobScheduler;
 import org.dimensinfin.logging.LogWrapper;
 
-import static org.dimensinfin.eveonline.neocom.infinity.backend.scheduler.config.CronSchedulePropertyDefinitions.CRON_SCHEDULE_PROCESSING_BLUEPRINTS;
+import static org.dimensinfin.eveonline.neocom.infinity.backend.scheduler.config.CronSchedulePropertyNameDefinitions.CRON_SCHEDULE_PROCESSING_BLUEPRINTS_PROPERTY_NAME;
 
 //@Component
 public class ScheduleBlueprintProcessor {
 	private final IConfigurationService configurationService;
 	private final ESIDataService esiDataService;
 
+// - C O N S T R U C T O R S
 	public ScheduleBlueprintProcessor( final @NotNull IConfigurationService configurationService,
 	                                   final @NotNull ESIDataService esiDataService ) {
 		this.configurationService = configurationService;
-		this.esiDataService = esiDataService;}
+		this.esiDataService = esiDataService;
+	}
 
 	/**
 	 * This method is executed after the application starts and registers the credential hob generator into the minute scheduler.
 	 */
-//	@LogEnterExit
-//	@EventListener(ApplicationReadyEvent.class)
+	//	@LogEnterExit
+	//	@EventListener(ApplicationReadyEvent.class)
 	public void registerCredentialJobGenerator() {
 		LogWrapper.enter();
 		JobScheduler.getJobScheduler().registerJob( new BlueprintProcessorJob.Builder()
 				.withEsiDataService( this.esiDataService )
-				.addCronSchedule( this.configurationService.getResourceString( CRON_SCHEDULE_PROCESSING_BLUEPRINTS, "* - 0" ) )
+				.addCronSchedule( this.configurationService.getResourceString( CRON_SCHEDULE_PROCESSING_BLUEPRINTS_PROPERTY_NAME, "* - 0" ) )
 				.build() );
 		LogWrapper.exit();
 	}
--- NeoCom-Infinity-Backend/src/main/java/org/dimensinfin/eveonline/neocom/infinity/backend/scheduler/config/CronSchedulePropertyDefinitions.java
+++
@@ -1,7 +0,0 @@
-package org.dimensinfin.eveonline.neocom.infinity.backend.scheduler.config;
-
-public class CronSchedulePropertyDefinitions {
-	public static final String CRON_SCHEDULE_CREDENTIAL_JOB_GENERATOR="P.scheduler.cron.credentialjobgenerator.schedule";
-	public static final String CRON_SCHEDULE_MINING_EXTRACTIONS="P.scheduler.cron.miningextractions.schedule";
-	public static final String CRON_SCHEDULE_PROCESSING_BLUEPRINTS="P.scheduler.cron.processingblueprints.schedule";
-}
