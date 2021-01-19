60d9ec93c8821857bae906e2f503eeed0ce1f37a
Adam Antinoo <adamantinoo.git@gmail.com>
2021-01-19 17:55:19 +0100

    -[BACKEND] Completing the initial public endpoints so frontend references do not become circular referernces.
    Former-commit-id: f9e43e75fd44a1713527fec09de6100c3a6e4374

--- NeoCom-Infinity-Backend/src/main/java/org/dimensinfin/eveonline/neocom/infinity/backend/universe/corporation/rest/v1/PublicCorporationControllerV1.java
+++ NeoCom-Infinity-Backend/src/main/java/org/dimensinfin/eveonline/neocom/infinity/backend/universe/corporation/rest/v1/PublicCorporationControllerV1.java
@@ -2,6 +2,7 @@ package org.dimensinfin.eveonline.neocom.infinity.backend.universe.corporation.r
 
 import javax.validation.constraints.NotNull;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.dimensinfin.eveonline.neocom.domain.Corporation;
+import org.dimensinfin.eveonline.neocom.infinity.backend.universe.domain.CorporationPublicDataV1;
 import org.dimensinfin.eveonline.neocom.infinity.corporation.rest.CorporationServiceV1;
 
 /**
@@ -18,19 +19,19 @@ import org.dimensinfin.eveonline.neocom.infinity.corporation.rest.CorporationSer
  */
 @RestController
 @Validated
-@RequestMapping("/api/v1/universe")
-public class UniverseCorporationControllerV1 {
+@RequestMapping("/api/v1/public")
+public class PublicCorporationControllerV1 {
 	private final CorporationServiceV1 corporationServiceV1;
 
 	// - C O N S T R U C T O R S
-	public UniverseCorporationControllerV1( @NotNull final CorporationServiceV1 corporationServiceV1 ) {
+	public PublicCorporationControllerV1( @NotNull final CorporationServiceV1 corporationServiceV1 ) {
 		this.corporationServiceV1 = corporationServiceV1;
 	}
 
 	@GetMapping(path = "/corporations/{corporationId}",
 			consumes = "application/json",
 			produces = "application/json")
-	public ResponseEntity<Corporation> getCorporationData( @PathVariable final Integer corporationId ) {
-		return this.corporationServiceV1.getCorporationData( corporationId );
+	public ResponseEntity<CorporationPublicDataV1> getCorporationData( @PathVariable @NotNull final Integer corporationId ) {
+		return new ResponseEntity<>( this.corporationServiceV1.getCorporationPublicData( corporationId ), HttpStatus.OK );
 	}
 }
