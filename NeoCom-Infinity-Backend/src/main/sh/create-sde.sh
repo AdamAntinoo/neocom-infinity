fe4fdb6b27a445a514f8c267d8063b3028b9552b
Adam Antinoo <adamantinoo.git@gmail.com>
2021-01-22 19:31:23 +0100

    -[PROJECT] Updated project tasks for backend additional test sourcesets.
    Former-commit-id: a43e574e1ad5f6fc99b02c2349f2850c81bc348e

--- NeoCom-Infinity-Backend/src/main/sh/create-sde.sh
+++ NeoCom-Infinity-Backend/src/main/sh/create-sde.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 figlet CreateSDE
 echo "> Database creation started."
-mv ./downloads/*.csv .
+cp ./downloads/*.csv .
 
 echo ">> Remove current sde database..."
 rm sde.db
 echo ">> Create new sde database..."
-/usr/bin/sqlite3 sde.db < db-ddl-0.20.0.ddl
+/usr/bin/sqlite3 sde.db <db-ddl-0.20.0.ddl
 cp sde.db ../resources/
 echo "> Database creation completed."
 rm -rf *.csv
