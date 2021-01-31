c4463ae524695a160d12c32ec88f615ff2f74765
Adam Antinoo <adamantinoo.git@gmail.com>
2021-01-31 21:07:35 +0100

    -[PROJECT] Upgrading the docker configurations.
    Former-commit-id: b2fae914b105bbeaebec8453ab1b644853871911

--- NeoCom-Infinity-Backend/docker-service.sh
+++ NeoCom-Infinity-Backend/docker-service.sh
@@ -2,9 +2,8 @@
 # - PARAMETERS & CONSTANTS
 COMMAND=$1
 ENVIRONMENT=$2
-if [ -z "$ENVIRONMENT" ]
-then
-    ENVIRONMENT="acceptance"
+if [ -z "$ENVIRONMENT" ]; then
+  ENVIRONMENT="acceptance"
 fi
 figlet "Docker Service ${COMMAND}"
 echo
@@ -12,11 +11,11 @@ echo "Using environment: ${ENVIRONMENT}"
 APPLICATION_JAR_NAME="neocom-infinity-backend-container"
 WORKING_DIRECTORY="$(dirname "$0")"
 DOCKER_DIRECTORY="${WORKING_DIRECTORY}/src/main/resources/docker"
-DOCKER_COMPOSER_COMMAND="docker-compose --file src/test/scripts/docker-${ENVIRONMENT}/docker-compose.yml"
+DOCKER_COMPOSER_COMMAND="docker-compose --file src/test/scripts/neocom-docker-${ENVIRONMENT}/docker-compose.yml"
 
 # - G E N E R A T E   C O N T A I N E R
 generateContainer() {
-  cd "${WORKING_DIRECTORY}" || exit 1;
+  cd "${WORKING_DIRECTORY}" || exit 1
   # Cleanup
   rm -v "${DOCKER_DIRECTORY}"/*.jar
   rm -vrf "${DOCKER_DIRECTORY}"/properties*
@@ -25,31 +24,31 @@ generateContainer() {
   cp ./build/resources/main/app-banner.txt "$DOCKER_DIRECTORY"
   cp ./build/resources/main/app-banner.txt ./build/libs/app-banner.txt
   cp ./src/main/resources/sde.db "${DOCKER_DIRECTORY}"
- # Copy all properties sets into the container. Configuration will select the right set.
+  # Copy all properties sets into the container. Configuration will select the right set.
   cp -r ./src/main/resources/properties* "${DOCKER_DIRECTORY}"
-  cd "$DOCKER_DIRECTORY" || exit 1;
+  cd "$DOCKER_DIRECTORY" || exit 1
   mv -v NeoCom.Infinity.Backend*.jar ${APPLICATION_JAR_NAME}".jar"
   echo "${DOCKER_DIRECTORY}/Dockerfile"
   docker build -t neocom/neocom-infinity.backend .
 }
 # - S T A R T / S T O P
 start() {
-  cd "${WORKING_DIRECTORY}" || exit 1;
+  cd "${WORKING_DIRECTORY}" || exit 1
   RUN_COMMAND="${DOCKER_COMPOSER_COMMAND}"
   $RUN_COMMAND up &
 }
 stop() {
-  cd "${WORKING_DIRECTORY}" || exit 1;
+  cd "${WORKING_DIRECTORY}" || exit 1
   RUN_COMMAND="${DOCKER_COMPOSER_COMMAND}"
   $RUN_COMMAND down
 }
 recycle() {
   # - STOP
-  cd "${WORKING_DIRECTORY}" || exit 1;
+  cd "${WORKING_DIRECTORY}" || exit 1
   RUN_COMMAND="${DOCKER_COMPOSER_COMMAND}"
   $RUN_COMMAND down
   generateContainer
-  cd "${WORKING_DIRECTORY}" || exit 1;
+  cd "${WORKING_DIRECTORY}" || exit 1
   RUN_COMMAND="${DOCKER_COMPOSER_COMMAND}"
   $RUN_COMMAND up &
 }
