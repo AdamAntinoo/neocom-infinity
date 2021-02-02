93e44ddead908ea9547aec855e180eae5ae945c0
Adam Antinoo <adamantinoo.git@gmail.com>
2021-02-02 17:06:49 +0100

    -[FRONTEND] Fixing the rendering for Market Data.
    Former-commit-id: d53d128c3d6b6d979a77b9e4a898c4c5cc1ff066

--- NeoCom-Infinity-Frontend/src/app/domain/esi/EsiMarketData.esi.ts
+++ NeoCom-Infinity-Frontend/src/app/domain/esi/EsiMarketData.esi.ts
@@ -5,7 +5,8 @@ import { EsiNode } from "./EsiNode.esi"
 import { UniverseMarketOrder } from "./UniverseMarketOrder.esi"
 
 export class EsiMarketData extends EsiNode {
-    private bestSellOrder: MarketOrderDto
+    public typeId:number
+    public bestSellOrder: MarketOrderDto
     private bestBuyOrder: MarketOrderDto
     private sellOrders: UniverseMarketOrder[]
     private sellDeep: number
