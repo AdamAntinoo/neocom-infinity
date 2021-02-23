a7d7f6b4ea1542dcbe582184592d2fb069a3b875
Adam Antinoo <adamantinoo.git@gmail.com>
2021-02-23 12:34:53 +0100

    -[BACKEND][BUG] The Description field at the EsiType is missing.
    Former-commit-id: dab248c481c474abae9817732f59b530e35d5a39

--- NeoCom-Infinity-Backend/src/test/java/org/dimensinfin/eveonline/neocom/infinity/backend/universe/serializer/EsiTypeSerializerTest.java
+++ NeoCom-Infinity-Backend/src/test/java/org/dimensinfin/eveonline/neocom/infinity/backend/universe/serializer/EsiTypeSerializerTest.java
@@ -17,6 +17,7 @@ import org.dimensinfin.eveonline.neocom.esiswagger.model.GetUniverseTypesTypeIdO
 public class EsiTypeSerializerTest {
 	public static final Integer TEST_ESITYPE_ID = 11535;
 	public static final String TEST_ESITYPE_NAME = "Magnetometric Sensor Cluster";
+	public static final String TEST_ESITYPE_DESCRIPTION = "Magnetometric Sensor Cluster";
 	public static final String TEST_ESITYPE_GROUP_NAME = "Construction Components";
 	public static final String TEST_ESITYPE_CATEGORY_NAME = "Commodity";
 	public static final String TEST_ESITYPE_TYPEICON_URL = "https://image.eveonline.com/Type/11535_64.png";
@@ -45,6 +46,7 @@ public class EsiTypeSerializerTest {
 	public void serialize() throws JsonProcessingException {
 		// When
 		Mockito.when( this.type.getName() ).thenReturn( TEST_ESITYPE_NAME );
+		Mockito.when( this.type.getDescription() ).thenReturn( TEST_ESITYPE_DESCRIPTION );
 		Mockito.when( this.type.getVolume() ).thenReturn( TEST_ESITYPE_VOLUME );
 		Mockito.when( this.category.getName() ).thenReturn( TEST_ESITYPE_CATEGORY_NAME );
 		// Test
@@ -55,7 +57,7 @@ public class EsiTypeSerializerTest {
 				.withCategory( this.category )
 				.build();
 		final String json = this.objectMapper.writeValueAsString( esiType );
-		final String expected = "{\"typeId\":11535,\"name\":\"Magnetometric Sensor Cluster\",\"group\":{\"categoryId\":0,\"groupId\":0,\"name\":null,\"published\":false,\"types\":[]},\"category\":{\"categoryId\":0,\"groups\":[],\"name\":\"Commodity\",\"published\":false},\"type\":{\"capacity\":0.0,\"description\":null,\"dogmaAttributes\":[],\"dogmaEffects\":[],\"graphicId\":0,\"groupId\":0,\"iconId\":0,\"marketGroupId\":0,\"mass\":0.0,\"name\":\"Magnetometric Sensor Cluster\",\"packagedVolume\":0.0,\"portionSize\":0,\"published\":false,\"radius\":0.0,\"typeId\":0,\"volume\":0.1},\"tech\":\"Tech I\",\"volume\":0.1,\"isBlueprint\":false,\"typeIconURL\":\"https://image.eveonline.com/Type/11535_64.png\",\"marketData\":{\"rel\":\"marketData\",\"href\":\"/api/v1/universe/market/consolidated/byregion/10000043/11535\"}}";
+		final String expected = "{\"typeId\":11535,\"name\":\"Magnetometric Sensor Cluster\",\"description\":\"Magnetometric Sensor Cluster\",\"group\":{\"categoryId\":0,\"groupId\":0,\"name\":null,\"published\":false,\"types\":[]},\"category\":{\"categoryId\":0,\"groups\":[],\"name\":\"Commodity\",\"published\":false},\"type\":{\"capacity\":0.0,\"description\":\"Magnetometric Sensor Cluster\",\"dogmaAttributes\":[],\"dogmaEffects\":[],\"graphicId\":0,\"groupId\":0,\"iconId\":0,\"marketGroupId\":0,\"mass\":0.0,\"name\":\"Magnetometric Sensor Cluster\",\"packagedVolume\":0.0,\"portionSize\":0,\"published\":false,\"radius\":0.0,\"typeId\":0,\"volume\":0.1},\"tech\":\"Tech I\",\"volume\":0.1,\"isBlueprint\":false,\"typeIconURL\":\"https://image.eveonline.com/Type/11535_64.png\",\"marketData\":{\"rel\":\"marketData\",\"href\":\"/api/v1/universe/market/consolidated/byregion/10000043/11535\"}}";
 		Assertions.assertEquals( expected, json );
 	}
 }
