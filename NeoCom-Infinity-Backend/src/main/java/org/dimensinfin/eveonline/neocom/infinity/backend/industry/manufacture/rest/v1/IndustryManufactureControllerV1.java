0e14de390e2c749f9455b9782668d10fb7e27c57
Adam Antinoo <adamantinoo.git@gmail.com>
2021-03-05 19:44:36 +0100

    -[BACKEND] Adapted to new injection and removed obsolete code.
    Former-commit-id: 9a49893a57e03fa17a00893d0ffa522a19d9240d

--- NeoCom-Infinity-Backend/src/main/java/org/dimensinfin/eveonline/neocom/infinity/backend/industry/manufacture/rest/v1/IndustryManufactureControllerV1.java
+++ NeoCom-Infinity-Backend/src/main/java/org/dimensinfin/eveonline/neocom/infinity/backend/industry/manufacture/rest/v1/IndustryManufactureControllerV1.java
@@ -13,8 +13,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.dimensinfin.eveonline.neocom.industry.domain.ProcessedBlueprint;
 import org.dimensinfin.eveonline.neocom.infinity.backend.industry.IndustryControllerV1;
-import org.dimensinfin.eveonline.neocom.infinity.backend.industry.domain.ProcessedBlueprint;
 import org.dimensinfin.eveonline.neocom.infinity.config.security.NeoComAuthenticationProvider;
 
 @RestController
--- NeoCom-Infinity-Backend/src/main/java/org/dimensinfin/eveonline/neocom/infinity/backend/industry/manufacture/rest/v1/IndustryManufactureServiceV1.java
+++ NeoCom-Infinity-Backend/src/main/java/org/dimensinfin/eveonline/neocom/infinity/backend/industry/manufacture/rest/v1/IndustryManufactureServiceV1.java
@@ -4,25 +4,27 @@ import java.util.List;
 import java.util.Objects;
 import javax.validation.constraints.NotNull;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import org.dimensinfin.eveonline.neocom.infinity.backend.industry.domain.ProcessedBlueprint;
+import org.dimensinfin.eveonline.neocom.industry.domain.ProcessedBlueprint;
 import org.dimensinfin.eveonline.neocom.infinity.config.security.CredentialDetailsService;
 import org.dimensinfin.eveonline.neocom.infinity.config.security.NeoComAuthenticationProvider;
 import org.dimensinfin.eveonline.neocom.infinity.core.ExceptionMessagesExternalisedType;
 import org.dimensinfin.eveonline.neocom.infinity.core.exception.NeoComRuntimeBackendException;
 import org.dimensinfin.eveonline.neocom.infinity.core.rest.NeoComAuthenticatedService;
-import org.dimensinfin.eveonline.neocom.infinity.service.DataStoreService;
+import org.dimensinfin.eveonline.neocom.service.IDataStore;
 import org.dimensinfin.logging.LogWrapper;
 
 @Service
 public class IndustryManufactureServiceV1 extends NeoComAuthenticatedService {
-	private final DataStoreService dataStoreService;
+	private final IDataStore dataStoreService;
 
 	// - C O N S T R U C T O R S
+	@Autowired
 	public IndustryManufactureServiceV1( @NotNull final NeoComAuthenticationProvider neoComAuthenticationProvider,
 	                                     @NotNull final CredentialDetailsService credentialDetailsService,
-	                                     @NotNull final DataStoreService dataStoreService ) {
+	                                     @NotNull final IDataStore dataStoreService ) {
 		super( neoComAuthenticationProvider, credentialDetailsService );
 		this.dataStoreService = dataStoreService;
 	}
@@ -31,7 +33,7 @@ public class IndustryManufactureServiceV1 extends NeoComAuthenticatedService {
 	public List<ProcessedBlueprint> getBlueprints4PilotWithCostIndex( @NotNull final Integer pilotId ) {
 		try {
 			return this.dataStoreService.accessProcessedBlueprints( Objects.requireNonNull(
-					this.getAuthorizedCredential()
+					this.getAuthorizedCredential().getAccountId()
 			) );
 		} catch (final NullPointerException npe) {
 			LogWrapper.error( npe );
