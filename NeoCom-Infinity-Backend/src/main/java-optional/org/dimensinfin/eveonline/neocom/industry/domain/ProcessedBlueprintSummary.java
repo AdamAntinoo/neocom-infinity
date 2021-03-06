2b0bab7334f2da0bc9baaf46216fac3140493572
Adam Antinoo <adamantinoo.git@gmail.com>
2021-03-06 17:29:34 +0100

    -[BACKEND] New endpoint classes for reduced output to the front end. Now blueprint detail data is on demand.
    Former-commit-id: a58f21f72b3dbaa4609152dd8a15427ddabbc1c4

---
+++ NeoCom-Infinity-Backend/src/main/java-optional/org/dimensinfin/eveonline/neocom/industry/domain/ProcessedBlueprintSummary.java
@@ -0,0 +1,132 @@
+package org.dimensinfin.eveonline.neocom.industry.domain;
+
+import org.dimensinfin.eveonline.neocom.domain.EsiType;
+import org.dimensinfin.eveonline.neocom.domain.space.Station;
+import org.dimensinfin.eveonline.neocom.utility.NeoObjects;
+
+/**
+ * Contains an abstract for the full processed blueprint data. Enough to report the user the quality of the blueprint but not all the details.
+ * If a user has dozens of blueprints the amount of data to transfer and process will be high and time consuming. Better transfer an abstract and
+ * then select a target that will retrieve the full data.
+ *
+ * @author Adam Antinoo (adamantinoo.git@gmail.com)
+ * @since 0.20.0
+ */
+public class ProcessedBlueprintSummary {
+	private String uid;
+	private Integer blueprintTypeId;
+	private String blueprintTypeName;
+	private String blueprintTypeIconURL;
+	private Integer outputTypeId;
+	private String outputTypeName;
+	private String outputTypeIconURL;
+	private Double outputPrice;
+	private Station tradeStation;
+	private Double manufactureMaterialCost;
+	private Double costIndex;
+
+	// - C O N S T R U C T O R S
+	private ProcessedBlueprintSummary() {}
+
+	// - G E T T E R S   &   S E T T E R S
+	public String getBlueprintTypeIconURL() {
+		return this.blueprintTypeIconURL;
+	}
+
+	public Integer getBlueprintTypeId() {
+		return this.blueprintTypeId;
+	}
+
+	public String getBlueprintTypeName() {
+		return this.blueprintTypeName;
+	}
+
+	public Double getCostIndex() {
+		return this.costIndex;
+	}
+
+	public Double getManufactureMaterialCost() {
+		return this.manufactureMaterialCost;
+	}
+
+	public Double getOutputPrice() {
+		return this.outputPrice;
+	}
+
+	public String getOutputTypeIconURL() {
+		return this.outputTypeIconURL;
+	}
+
+	public Integer getOutputTypeId() {
+		return this.outputTypeId;
+	}
+
+	public String getOutputTypeName() {
+		return this.outputTypeName;
+	}
+
+	public Station getTradeStation() {
+		return this.tradeStation;
+	}
+
+	public String getUid() {
+		return this.uid;
+	}
+
+	// - B U I L D E R
+	public static class Builder {
+		private final ProcessedBlueprintSummary onConstruction;
+
+		// - C O N S T R U C T O R S
+		public Builder() {
+			this.onConstruction = new ProcessedBlueprintSummary();
+		}
+
+		public ProcessedBlueprintSummary build() {
+			NeoObjects.requireNonNull( this.onConstruction.uid );
+			NeoObjects.requireNonNull( this.onConstruction.tradeStation );
+			NeoObjects.requireNonNull( this.onConstruction.manufactureMaterialCost );
+			NeoObjects.requireNonNull( this.onConstruction.costIndex );
+			return this.onConstruction;
+		}
+
+		public ProcessedBlueprintSummary.Builder withBlueprint( final EsiType blueprint ) {
+			if (null != blueprint) {
+				this.onConstruction.blueprintTypeId = blueprint.getTypeId();
+				this.onConstruction.blueprintTypeName = blueprint.getName();
+				this.onConstruction.blueprintTypeIconURL = blueprint.getTypeIconURL();
+			}
+			return this;
+		}
+
+		public ProcessedBlueprintSummary.Builder withCostIndex( final Double costIndex ) {
+			this.onConstruction.costIndex = NeoObjects.requireNonNull( costIndex );
+			return this;
+		}
+
+		public ProcessedBlueprintSummary.Builder withManufactureCost( final Double cost ) {
+			this.onConstruction.manufactureMaterialCost = NeoObjects.requireNonNull( cost );
+			return this;
+		}
+
+		public ProcessedBlueprintSummary.Builder withOutput( final PricedResource output ) {
+			if (null != output) {
+				this.onConstruction.outputTypeId = output.getTypeId();
+				this.onConstruction.outputTypeName = output.getName();
+				this.onConstruction.outputTypeIconURL = output.getTypeIconURL();
+				this.onConstruction.outputPrice = output.getMarketPrice();
+			}
+			return this;
+		}
+
+		public ProcessedBlueprintSummary.Builder withTradeStation( final Station station ) {
+			this.onConstruction.tradeStation = NeoObjects.requireNonNull( station );
+			return this;
+		}
+
+		public ProcessedBlueprintSummary.Builder withUID( final String uid ) {
+			this.onConstruction.uid = NeoObjects.requireNonNull( uid );
+			return this;
+		}
+	}
+}
---
+++ NeoCom-Infinity-Backend/src/main/java/org/dimensinfin/eveonline/neocom/infinity/backend/industry/converter/ProcessedBlueprintToSummaryConverter.java
@@ -0,0 +1,39 @@
+package org.dimensinfin.eveonline.neocom.infinity.backend.industry.converter;
+
+import javax.validation.constraints.NotNull;
+
+import org.dimensinfin.core.interfaces.Converter;
+import org.dimensinfin.eveonline.neocom.industry.domain.ProcessedBlueprint;
+import org.dimensinfin.eveonline.neocom.industry.domain.ProcessedBlueprintSummary;
+
+/**
+ * @author Adam Antinoo (adamantinoo.git@gmail.com)
+ * @since 0.20.0
+ */
+public class ProcessedBlueprintToSummaryConverter implements Converter<ProcessedBlueprint, ProcessedBlueprintSummary> {
+	private final String uniqueId;
+
+	// - C O N S T R U C T O R S
+	public ProcessedBlueprintToSummaryConverter( @NotNull final String uniqueId ) {
+		this.uniqueId = uniqueId;
+	}
+
+	@Override
+	public ProcessedBlueprintSummary convert( final ProcessedBlueprint input ) {
+		final double manufactureCost = input.getBom().stream()
+				.filter( element -> element.getCategory().getCategoryId() != 9 )
+				.filter( element -> element.getCategory().getCategoryId() != 16 )
+				.mapToDouble( element -> element.getMarketPrice() * element.getQuantity() )
+				.sum();
+		// TODO - Add the job build time to the cost index calculation.
+		final double costIndex = (input.getOutput().getMarketPrice() * input.getOutput().getQuantity()) * 0.9 / manufactureCost;
+		return new ProcessedBlueprintSummary.Builder()
+				.withUID( this.uniqueId )
+				.withBlueprint( input.getBlueprint() )
+				.withOutput( input.getOutput() )
+				.withTradeStation( input.getOutputMarketData().getBestSellOrder().getStation() )
+				.withManufactureCost( manufactureCost )
+				.withCostIndex( costIndex )
+				.build();
+	}
+}
