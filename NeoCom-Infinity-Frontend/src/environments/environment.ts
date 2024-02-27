4c7cd038e2b364cc78f65de7c190da0298815791
adam antinoo <adamantinoo.git@gmail.com>
2024-02-27 15:29:34 +0100

    -[FIX] Fix missing method on component.
    Former-commit-id: eb327e1ed045c85d861a119ba4a782afc594782f

--- NeoCom-Infinity-Frontend/src/environments/environment.ts
+++ NeoCom-Infinity-Frontend/src/environments/environment.ts
@@ -10,14 +10,14 @@ export const environment = {
     appName: require('../../package.json').name,
     appVersion: require('../../package.json').version + " dev",
     serverName: '',
-    loginLink: "https://login.eveonline.com/v2/oauth/authorize/?response_type=code&client_id=eacaa9cd36594189877544d851753734&state=LU5FT0NPTS5JTkZJTklUWS1QUk9EVUNUSU9OLVZBTElEIFNUQVRFIFNUUklORy0=&redirect_uri=http%3A%2F%2Flocalhost%3A5202%2Fapp%2FloginValidation&scope=publicData%20esi-corporations.read_structures.v1%20esi-characters.read_loyalty.v1%20esi-industry.read_character_jobs.v1%20esi-characters.read_corporation_roles.v1%20esi-industry.read_character_mining.v1",
+    loginLink: "https://login.eveonline.com/v2/oauth/authorize/?response_type=code&client_id=365122836bbb4f6d875aa5548426abf1&state=LU5FT0NPTS5JTkZJTklUWS1QUk9EVUNUSU9OLVZBTElEIFNUQVRFIFNUUklORy0=&redirect_uri=http%3A%2F%2Flocalhost%3A5200%2Fapp%2FloginValidation&scope=publicData%20esi-wallet.read_character_wallet.v1%20esi-industry.read_character_jobs.v1%20esi-industry.read_character_mining.v1",
     platform: 'Angular 8.2.3 - RxJs 6.4.0 - Rollbar 2.13',
     appSignature: "S0000.0020.0000",
 
     esiData : 'https://esi.evetech.net/latest/',
 
     mockStatus: true,
-    LoginRequest: 'https://login.eveonline.com/v2/oauth/authorize/?response_type=code&client_id=eacaa9cd36594189877544d851753734&state=LU5FT0NPTS5JTkZJTklUWS1ERVZFTE9QTUVOVC1WQUxJRCBTVEFURSBTVFJJTkct&redirect_uri=http%3A%2F%2Fneocom.infinity.local%2Fapp%2FloginValidation'
+    LoginRequest: 'https://login.eveonline.com/v2/oauth/authorize/?response_type=code&client_id=365122836bbb4f6d875aa5548426abf1&state=LU5FT0NPTS5JTkZJTklUWS1ERVZFTE9QTUVOVC1WQUxJRCBTVEFURSBTVFJJTkct&redirect_uri=http%3A%2F%2Fneocom.infinity.local%2Fapp%2FloginValidation'
 };
 
 import 'zone.js/dist/zone';  // Included with Angular CLI.
