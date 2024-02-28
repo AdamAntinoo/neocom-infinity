d612ea03b7016c9e64903aefc5d945ca661c0ff8
adam antinoo <adamantinoo.git@gmail.com>
2024-02-28 12:13:58 +0100

    -[TEST] Added some code to one step.

--- neocom-infinity-nest/test/acceptance/asset-esi.ts
+++ neocom-infinity-nest/test/acceptance/asset-esi.ts
@@ -1,3 +1,9 @@
-export class AssetEsi{
-  public id:number;
+export class AssetEsi {
+  public is_singleton: boolean;
+  public item_id: number;
+  public location_flag: string;
+  public location_id: number;
+  public location_type: string;
+  public quantity: number;
+  public type_id: number;
 }
