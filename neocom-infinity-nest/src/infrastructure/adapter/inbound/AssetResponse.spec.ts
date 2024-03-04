c6e4453a96d79fd0f0ac7722e5dd6431129fc124
adam antinoo <adamantinoo.git@gmail.com>
2024-03-04 19:10:06 +0100

    -[PROJECT] Remove unused files.

--- neocom-infinity-nest/src/infrastructure/adapter/inbound/AssetResponse.spec.ts
+++
@@ -1,9 +0,0 @@
-import { Test } from "@nestjs/testing";
-
-describe('DTO AssetResponse [Module: Assets]', () => {
-	describe('constructor contract', () => {
-		it('should be created', () => {
-			//expect(component).toBeDefined('service has not been created.')
-		})
-	});
-});
