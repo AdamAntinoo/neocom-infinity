4fb088027f6b9449f59bcb9115df987cc60f4655
adam antinoo <adamantinoo.git@gmail.com>
2024-03-06 19:10:54 +0100

    -[PROJECT] Saving new Visual Code configuration to repository.

--- neocom-infinity-nest/.eslintrc.js
+++ neocom-infinity-nest/.eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  code: 150,
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
@@ -21,5 +22,14 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    "@typescript-eslint/no-namespace": "off"
   },
-};
+  overrides: [
+    {
+      files: ['*.ts'],
+      rules: {
+        semi: ['error', 'never'],
+      }
+    }
+  ]
+}
