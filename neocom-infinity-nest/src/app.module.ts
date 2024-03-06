933d9c24d3e7d973ecdc41b8ab9e00e1c11bd981
adam antinoo <adamantinoo.git@gmail.com>
2024-03-06 19:11:23 +0100

    -[CHORE] Export components on modules.

--- neocom-infinity-nest/src/app.module.ts
+++ neocom-infinity-nest/src/app.module.ts
@@ -2,13 +2,17 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [HttpModule.register({
-    timeout: 5000,
-    maxRedirects: 5
-  })],
+  imports: [
+    HttpModule.register({
+      timeout: 5000,
+      maxRedirects: 5
+    }),
+    ScheduleModule.forRoot()
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
