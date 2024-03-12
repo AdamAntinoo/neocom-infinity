16d22506849076c845ee3a925b336502596409e0
adam antinoo <adamantinoo.git@gmail.com>
2024-03-12 11:20:52 +0100

    -[PROJECT] Initial Cucumber setup. Does not work.

--- NeoCom-Infinity-Frontend/cucumber/worlds/NIF05World.ts
+++ NeoCom-Infinity-Frontend/cucumber/worlds/NIF05World.ts
@@ -1,5 +1,7 @@
-import { Credential } from "@domain/core/Credential.domain";
+import { Credential } from "@domain/core/Credential.domain"
+import { ESISecureDataServiceAdapter } from "@infra/adapters/outbound/esi-data-service/ESISecureDataServiceAdapter"
 
 export class NIF05World {
     public credential: Credential
+    public esiAdapter : ESISecureDataServiceAdapter
 }
