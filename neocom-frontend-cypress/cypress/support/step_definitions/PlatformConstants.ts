994dc96dfecb2a8a47bc61b0bc0622e316ce13be
adam antinoo <adamantinoo.git@gmail.com>
2024-03-14 00:38:27 +0100

    -[FEAT] Add the initial steps and feature to test the Mining Operations page. Added authentication.

---
+++ neocom-frontend-cypress/cypress/support/step_definitions/PlatformConstants.ts
@@ -0,0 +1,13 @@
+export const PlatformConstants = {
+    // - C O N S T A N T S
+    NEOCOM_V1: '/api/v1/neocom',
+    NEOCOM_V2: '/api/v2/neocom',
+    UNIVERSE_V1: '/api/v1/universe',
+    JWTTOKEN_KEY: '-JWTTOKEN_KEY-',
+    JWTTOKEN_EXPIRATION_TIME_KEY: '-JWTTOKEN_EXPIRATION_TIME_KEY-',
+    CREDENTIAL_KEY: '-CREDENTIAL-KEY-',
+    DEFAULT_VARIANT: '-DEFAULT-',
+    DEFAULT_FEATURE_REF: 'assets/media/default-feature.jpeg',
+    DEFAULT_FEATURE_LABEL: '-',
+    VALID_SESSION_STATE: 'VALID'
+}
