4f1e192c5972e9b41153604f81f1ad1ea6c4b44b
adam antinoo <adamantinoo.git@gmail.com>
2024-03-14 10:14:25 +0100

    -[PROJECT] Add new Cypress feature steps from separate project.

--- NeoCom-Infinity-Frontend/cypress.config.ts
+++ NeoCom-Infinity-Frontend/cypress.config.ts
@@ -1,62 +1,29 @@
 import { defineConfig } from "cypress";
-import * as webpack from "@cypress/webpack-preprocessor";
 import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
+import { preprocessor } from "@badeball/cypress-cucumber-preprocessor/browserify";
 
 async function setupNodeEvents(
-    on: Cypress.PluginEvents,
-    config: Cypress.PluginConfigOptions
+  on: Cypress.PluginEvents,
+  config: Cypress.PluginConfigOptions
 ): Promise<Cypress.PluginConfigOptions> {
-    // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
-    await addCucumberPreprocessorPlugin(on, config);
+  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+  await addCucumberPreprocessorPlugin(on, config);
 
-    on(
-        "file:preprocessor",
-        webpack({
-            webpackOptions: {
-                resolve: {
-                    extensions: [".ts", ".js"],
-                },
-                module: {
-                    rules: [
-                        {
-                            test: /\.ts$/,
-                            exclude: [/node_modules/],
-                            use: [
-                                {
-                                    loader: "ts-loader",
-                                },
-                            ],
-                        },
-                        {
-                            test: /\.feature$/,
-                            use: [
-                                {
-                                    loader: "@badeball/cypress-cucumber-preprocessor/webpack",
-                                    options: config,
-                                },
-                            ],
-                        },
-                    ],
-                },
-            },
-        })
-    );
+  on(
+    "file:preprocessor",
+    preprocessor(config, {
+      typescript: require.resolve("typescript"),
+    })
+  );
 
-    // Make sure to return the config object as it might have been modified by the plugin.
-    return config;
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
 }
 
 export default defineConfig({
-    component: {
-        devServer: {
-            framework: "angular",
-            bundler: "webpack",
-        },
-        specPattern: "cypress/**/*.feature",
-        setupNodeEvents
-    },
-    e2e: {
-        specPattern: "cypress/**/*.feature",
-        setupNodeEvents
-    }
+  e2e: {
+    baseUrl: "http://localhost:5200",
+    specPattern: "**/*.feature",
+    setupNodeEvents,
+  },
 });
---
+++ NeoCom-Infinity-Frontend/cypress/support/step_definitions/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+    production: false,
+    showexceptions: true,
+    copyright: '© 2019,2024 Dimensinfin Industries',
+    appTitle: 'NeoCom Infinity',
+    appName: 'App Name',
+    appVersion: "0.21.0 dev",
+    serverName: '',
+    loginLink: "https://login.eveonline.com/v2/oauth/authorize/?response_type=code&client_id=365122836bbb4f6d875aa5548426abf1&state=LU5FT0NPTS5JTkZJTklUWS1QUk9EVUNUSU9OLVZBTElEIFNUQVRFIFNUUklORy0=&redirect_uri=http%3A%2F%2Flocalhost%3A5200%2Fapp%2FloginValidation&scope=publicData%20esi-wallet.read_character_wallet.v1%20esi-industry.read_character_jobs.v1%20esi-industry.read_character_mining.v1",
+    platform: 'Angular 8.2.3 - RxJs 6.4.0 - Rollbar 2.13',
+    appSignature: "S0000.0020.0000",
+
+    esiData : 'https://esi.evetech.net/latest/',
+
+    mockStatus: true,
+    LoginRequest: 'https://login.eveonline.com/v2/oauth/authorize/?response_type=code&client_id=365122836bbb4f6d875aa5548426abf1&state=LU5FT0NPTS5JTkZJTklUWS1ERVZFTE9QTUVOVC1WQUxJRCBTVEFURSBTVFJJTkct&redirect_uri=http%3A%2F%2Fneocom.infinity.local%2Fapp%2FloginValidation'
+};
+
+// import 'zone.js/dist/zone';  // Included with Angular CLI.
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
