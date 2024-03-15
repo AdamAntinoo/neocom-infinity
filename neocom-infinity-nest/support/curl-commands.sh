b7592f48dd46766bdfde690ab3f85529e9365a43
adam antinoo <adamantinoo.git@gmail.com>
2024-03-15 16:43:20 +0100

    -[PROJECT] Update mock data and configuration for testing.

---
+++ neocom-infinity-nest/support/curl-commands.sh
@@ -0,0 +1,5 @@
+# Asset list
+curl --verbose --location --request GET "http://localhost:5271/characters/10001/assets/"
+
+# Mining Operations
+curl --verbose --location --request GET "http://localhost:5271/characters/{character_id}/mining/"
