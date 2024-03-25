c3fcbae3658accb577cb9693f5287f0d45a4c0c9
adam antinoo <adamantinoo.git@gmail.com>
2024-03-25 12:44:43 +0100

    -[FEAT] Complete the missing functionality and add unit testing for most of the new code.

--- neocom-infinity-nest/src/infrastructure/adapter/outbound/ESISecureDataServices/converter/MiningOperationConverter.ts
+++ neocom-infinity-nest/src/infrastructure/adapter/outbound/ESISecureDataServices/converter/MiningOperationConverter.ts
@@ -1,16 +1,19 @@
 import { V2MiningOperation } from "@Domain/entities/V2.MiningOperation";
 import { IConverter } from "@Inno/converter/IConverter.interface";
 import { IESIMiningOperation } from "../domain/IESIMiningOperation.interface";
+import { ESISecureDataServiceHALGeneratorAdapter } from "../esi.securedataservice.halgenerator.adapter";
 
 export class MiningOperationConverter<S, D> implements IConverter<IESIMiningOperation, V2MiningOperation>{
+    constructor(private halGenerator: ESISecureDataServiceHALGeneratorAdapter){}
+
     public convert(source: IESIMiningOperation): V2MiningOperation {
         let solarSystem: string = undefined
         let type: string = undefined
         if (source.solar_system_id) {
-            solarSystem = 'http://localhost'
+            solarSystem = this.halGenerator.getSystemLink(source.solar_system_id)
         }
         if (source.type_id) {
-            type = 'jhjhwfh'
+            type = this.halGenerator.getTypeLink(source.type_id)
         }
         return new V2MiningOperation({
             id: source.date + '-' + source.solar_system_id + '-' + source.type_id,
--- neocom-infinity-nest/src/infrastructure/adapter/outbound/ESISecureDataServices/esi.mining.adapter.ts
+++ neocom-infinity-nest/src/infrastructure/adapter/outbound/ESISecureDataServices/esi.mining.adapter.ts
@@ -7,29 +7,35 @@ import { lastValueFrom, map } from "rxjs"
 import { MiningOperationConverter } from "./converter/MiningOperationConverter"
 import { IESIMiningOperation } from "./domain/IESIMiningOperation.interface"
 import { TypedResponseTransformer } from "@Inno/transformer/TypedResponseTransformer"
+import { ESISecureDataServiceHALGeneratorAdapter } from "./esi.securedataservice.halgenerator.adapter"
+import { AxiosResponse } from "axios"
 
 @Injectable()
 export class EsiMiningAdapter implements EsiDataServicesPort {
     miningOperations: IEsiSecureService<V2MiningOperation>
 
-    constructor(private readonly httpService: HttpService) {
-        this.miningOperations = new EsiMiningSecureService(this.httpService)
+    constructor(private readonly httpService: HttpService,
+        private halGenerator: ESISecureDataServiceHALGeneratorAdapter) {
+        this.miningOperations = new EsiMiningSecureService(this.httpService, this.halGenerator)
     }
 }
 
 class EsiMiningSecureService implements IEsiSecureService<V2MiningOperation>{
-    constructor(private readonly httpService: HttpService) { }
+    constructor(private readonly httpService: HttpService,
+        private halGenerator: ESISecureDataServiceHALGeneratorAdapter) { }
 
     public async getMiningOperations(characterId: number): Promise<V2MiningOperation[]> {
         const request = 'http://localhost:5271' + '/characters/' + characterId + '/mining/'
         console.log('request->' + request)
 
         return await lastValueFrom(this.httpService.get<V2MiningOperation[]>(request)
-            .pipe(map((data: any) => {
-                console.log('esi data->' + JSON.stringify(data))
+            .pipe(map((axiosResponse: AxiosResponse) => {
+                console.log('esi data->' + axiosResponse.data)
+                console.log('esi data->' + axiosResponse.data.instanceof)
+                console.log('esi data->' + JSON.stringify(axiosResponse.data))
                 return new TypedResponseTransformer<V2MiningOperation>(V2MiningOperation)
-                    .setConverter(new MiningOperationConverter<IESIMiningOperation, V2MiningOperation>())
-                    .transform(data)
+                    .setConverter(new MiningOperationConverter<IESIMiningOperation, V2MiningOperation>(this.halGenerator))
+                    .transform(axiosResponse.data)
             }))
         )
     }
