9e4dbbd97cd2378e031a223b2479e8a30a77797a
adam antinoo <adamantinoo.git@gmail.com>
2024-03-26 12:50:37 +0100

    -[STORY] Remove the rest of unused files. -[STORY] Add cookie support.

--- neocom-infinity-nest/src/infrastructure/adapter/inbound/v1.miningoperations.controller.ts
+++ neocom-infinity-nest/src/infrastructure/adapter/inbound/v1.miningoperations.controller.ts
@@ -1,16 +1,14 @@
 import { CapsuleerMiningOperationsUseCase } from "@App/use-cases/mining-operation/CapsuleerMiningOperationsUseCase";
-import { EsiMining } from "@App/use-cases/mining-operation/EsiMining";
 import { V2MiningOperation } from "@Domain/entities/V2.MiningOperation";
-import { HttpService } from "@nestjs/axios";
+import { Cookies } from "@Infra/network/cookie.decorator";
 import { Controller, Get, Param } from "@nestjs/common";
-import { firstValueFrom } from "rxjs";
 
 @Controller('/nin/v1/character/')
 export class V1MiningOperationsController {
     constructor(private readonly getCapsuleerMiningOperationsUseCase: CapsuleerMiningOperationsUseCase) { }
 
     @Get(':characterId/miningoperations')
-    public async getMiningOperationsForCharacter(@Param() params: any): Promise<V2MiningOperation[]> {
+    public async getMiningOperationsForCharacter(@Cookies('name') token: string,@Param() params: any): Promise<V2MiningOperation[]> {
         return this.getCapsuleerMiningOperationsUseCase.getMiningOperations(params.characterId)
     }
 }
