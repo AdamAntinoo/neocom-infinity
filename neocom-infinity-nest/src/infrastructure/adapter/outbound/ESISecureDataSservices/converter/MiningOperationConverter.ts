a50ef6e766066bfdcc4c76b825dbae6346db4db6
adam antinoo <adamantinoo.git@gmail.com>
2024-03-27 10:08:17 +0100

    -[FEAT] Add the real HAL conversions while generating the MiningOperation.

--- neocom-infinity-nest/src/infrastructure/adapter/outbound/ESISecureDataSservices/converter/MiningOperationConverter.ts
+++ neocom-infinity-nest/src/infrastructure/adapter/outbound/ESISecureDataSservices/converter/MiningOperationConverter.ts
@@ -1,19 +1,16 @@
-import { V2MiningOperation } from "@Domain/entities/V2.MiningOperation"
-import { IConverter } from "@Inno/converter/IConverter.interface"
-import { IESIMiningOperation } from "../domain/IESIMiningOperation.interface"
-import { ESISecureDataServiceHALGeneratorAdapter } from "../esi.securedataservice.halgenerator.adapter"
+// import { V2MiningOperation } from "@Domain/entities/V2.MiningOperation";
+import { IConverter } from "@Domain/interfaces/IConverter.interface";
+import { IESIMiningOperation } from "../domain/IESIMiningOperation.interface";
 
 export class MiningOperationConverter<S, D> implements IConverter<IESIMiningOperation, V2MiningOperation>{
-    constructor(private halGenerator: ESISecureDataServiceHALGeneratorAdapter) { }
-
     public convert(source: IESIMiningOperation): V2MiningOperation {
         let solarSystem: string = undefined
         let type: string = undefined
         if (source.solar_system_id) {
-            solarSystem = this.halGenerator.getSystemLink(source.solar_system_id)
+            solarSystem = 'http://localhost'
         }
         if (source.type_id) {
-            type = this.halGenerator.getTypeLink(source.type_id)
+            type = 'jhjhwfh'
         }
         return new V2MiningOperation({
             id: source.date + '-' + source.solar_system_id + '-' + source.type_id,
