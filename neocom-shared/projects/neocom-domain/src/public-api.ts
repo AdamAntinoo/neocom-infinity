1cea4958ee2d6dd3968dfa2c226e5b898d4f29c6
adam antinoo <adamantinoo.git@gmail.com>
2024-04-01 17:22:18 +0200

    -[STORY] Save the old code when creating a shared library. The new code will be a standard Typescript commonJs library.

---
+++ neocom-shared/projects/neocom-domain/src/public-api.ts
@@ -0,0 +1,9 @@
+/*
+ * Public API Surface of neocom-domain
+ */
+
+export * from './transformer/TypedResponseTransformer'
+export * from './entities/V2.MiningOperation'
+export * from './core/ITransformable.interface'
+export * from './converter/IConverter.interface'
+export * from './converter/NoActionConverter'
