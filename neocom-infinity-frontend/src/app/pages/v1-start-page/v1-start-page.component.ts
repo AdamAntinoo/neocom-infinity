7d8d9af88cacb570ac274ed3d96d850a2ff29ab7
adam antinoo <adamantinoo.git@gmail.com>
2024-04-05 08:48:22 +0200

    -[CHORE] Fix the callback to the login validation but keeping the correct client id. Use the original configuration.

--- neocom-infinity-frontend/src/app/pages/v1-start-page/v1-start-page.component.ts
+++ neocom-infinity-frontend/src/app/pages/v1-start-page/v1-start-page.component.ts
@@ -4,11 +4,13 @@ import { OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 // - SERVICES
 import { BackendService } from '@app/services/backend.service';
-import { SessionStateResponse } from '@domain/dto/SessionStateResponse.dto';
-import { platformConstants } from '@env/platform-constants';
+import { AuthenticationStateResponse } from '@domain/dto/AuthenticationStateResponse.dto';
+import { environment } from '@env/environment';
+import { PlatformConstants } from '@env/PlatformConstants';
 // - INNOVATIVE
 import { BackgroundEnabledComponent } from '@innovative/components/background-enabled/background-enabled.component';
 import { IsolationService } from '@innovative/services/isolation.service';
+import { env } from 'process';
 
 @Component({
     selector: 'v1-start-page',
@@ -31,18 +33,31 @@ export class V1StartPageComponent extends BackgroundEnabledComponent implements 
      * If the session is not vald then we change the page to display the login button.
      */
     public ngOnInit(): void {
-        this.backendConnections.push(this.backendService.apiv1_ValidateAuthenticatedSession()
-            .subscribe((response: SessionStateResponse) => {
-                if (response.state == platformConstants.VALID_SESSION_STATE) this.pageChange('/dashboard')
-                else this.validating = false
+        this.backendConnections.push(this.backendService.apiv1_ValidateAuthtenticationState()
+            .subscribe((response: AuthenticationStateResponse) => {
+                if (response.state == PlatformConstants.VALID_SESSION_STATE) {
+                    console.log('-[V1StartPageComponent.ngOnInit]>Valid session')
+                    // Store the new tocken and credential
+                    this.isolationService.setToSession(PlatformConstants.JWTTOKEN_KEY, response.getJwtToken())
+                    this.isolationService.setToSession(PlatformConstants.CREDENTIAL_KEY, JSON.stringify(response.getCredential()))
+                    this.validating = false
+                    this.pageChange('/dashboard')
+                } else {
+                    console.log('-[V1StartPageComponent.ngOnInit]>Not Valid session')
+                    this.validating = false
+                }
             }, (error) => {
                 console.log('-[V1StartPageComponent.ngOnInit.exception]> Error message: ' + JSON.stringify(error.error))
                 setTimeout(() => this.validating = false, 1000)
             }))
     }
-    public getLoginLink(): string{
+    public getLoginLink(): string {
+        console.log('loginlink->' + environment.loginLink)
         return environment.loginLink
     }
+    // public v2getLoginLink(): string {
+    //     const link: string = environment.loginLinkData
+    // }
     private pageChange(route: string): void {
         console.log('><[ngOnInit.pageChange]> Route: ' + route);
         this.router.navigate([route]);
