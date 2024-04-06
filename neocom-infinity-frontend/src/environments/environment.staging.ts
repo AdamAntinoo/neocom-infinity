7684780b79baaa9e34f731cd1ba6bb4927d364f2
Adam Antinooº <adamantinoo.git@gmail.com>
2024-04-06 09:30:38 +0200

    -[FIX] Add new required scopes and fix the config map.

--- neocom-infinity-frontend/src/environments/environment.staging.ts
+++ neocom-infinity-frontend/src/environments/environment.staging.ts
@@ -15,7 +15,7 @@ export const environment = {
             state: "LU5FT0NPTS5JTkZJTklUWS1QUk9EVUNUSU9OLVZBTElEIFNUQVRFIFNUUklORy0="
         },
         callback_url: "http://localhost:32000/app/loginValidation",
-        scope: "publicData esi-location.read_ship_type.v1 esi-wallet.read_character_wallet.v1 esi-search.search_structures.v1 esi-universe.read_structures.v1 esi-assets.read_assets.v1 esi-planets.manage_planets.v1 esi-fittings.read_fittings.v1 esi-fittings.write_fittings.v1 esi-industry.read_character_jobs.v1 esi-markets.read_character_orders.v1 esi-characters.read_blueprints.v1 esi-contracts.read_character_contracts.v1 esi-corporations.read_blueprints.v1 esi-industry.read_character_mining.v1 esi-characterstats.read.v1"
+        scope: "publicData esi-location.read_ship_type.v1 esi-skills.read_skills.v1 esi-skills.read_skillqueue.v1 esi-wallet.read_character_wallet.v1 esi-search.search_structures.v1 esi-universe.read_structures.v1 esi-assets.read_assets.v1 esi-planets.manage_planets.v1 esi-fittings.read_fittings.v1 esi-fittings.write_fittings.v1 esi-industry.read_character_jobs.v1 esi-markets.read_character_orders.v1 esi-characters.read_blueprints.v1 esi-contracts.read_character_contracts.v1 esi-corporations.read_blueprints.v1 esi-industry.read_character_mining.v1 esi-characterstats.read.v1"
     },
     platform: 'Angular 13.4.0 - RxJs 7.5.0',
     appSignature: "S0000.0020.0001",
