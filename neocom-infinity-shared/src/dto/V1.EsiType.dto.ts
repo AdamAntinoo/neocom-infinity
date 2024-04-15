6cfb9c6fe91d79b2b78ab0e2c95d4eb468fda397
adam antinoo <adamantinoo.git@gmail.com>
2024-04-15 14:16:45 +0200

    -[FIX] Remove field initializations since that will block field definitions from object.

--- neocom-infinity-shared/src/dto/V1.EsiType.dto.ts
+++ neocom-infinity-shared/src/dto/V1.EsiType.dto.ts
@@ -1,15 +1,15 @@
 import { Record } from "../interfaces/Record.interface"
 
-export class V1EsiTypeDto extends Record{
-    public override jsonClass:string ='EsiType'
-    public typeId: number = 0 - 1
-    public name: string = ''
-    public description: string = ''
-    public iconId: number = -1
-    public groupId: number = -1
-    public groupName: string = ''
-    public categoryId: number = -1
-    public categoryName: string = ''
-    public volume: number = 0.01
-    public marketDataLink: string = 'link'
+export class V1EsiTypeDto extends Record {
+    public override jsonClass: string = 'EsiType'
+    public typeId?: number
+    public name?: string
+    public description?: string
+    public iconId?: number
+    public groupId?: number
+    public groupName?: string
+    public categoryId?: number
+    public categoryName?: string
+    public volume?: number
+    public marketDataLink?: string
 }
