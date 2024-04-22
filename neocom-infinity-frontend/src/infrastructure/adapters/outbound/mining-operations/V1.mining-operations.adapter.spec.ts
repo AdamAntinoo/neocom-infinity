972fc70bc3279ea1cd035b7f6ee3d1169f492237
adam antinoo <adamantinoo.git@gmail.com>
2024-04-22 19:34:10 +0200

    -[CHORE] Add dependencies and configuration to try to run it with Jest. No way.

--- neocom-infinity-frontend/src/infrastructure/adapters/outbound/mining-operations/V1.mining-operations.adapter.spec.ts
+++ neocom-infinity-frontend/src/infrastructure/adapters/outbound/mining-operations/V1.mining-operations.adapter.spec.ts
@@ -32,60 +32,60 @@ describe('ADAPTER V1MiningOperationsAdapterService [Module: Infrastructure]', ()
                             return Observable.create((observer) => {
                                 observer.next([
                                     {
-                                      "jsonClass": "MiningOperationDto",
-                                      "resources": [
-                                        {
-                                          "jsonClass": "StackDto",
-                                          "quantity": 10000,
-                                          "typeLink": "/esi/v1/universe/types/17464"
-                                        },
-                                        {
-                                          "jsonClass": "StackDto",
-                                          "quantity": 12000,
-                                          "typeLink": "/esi/v1/universe/types/1224"
-                                        }
-                                      ],
-                                      "id": "2024-02-25/30003538",
-                                      "date": "2024-02-25",
-                                      "solarSystemLink": "/esi/v1/universe/spacelocation/30003538"
+                                        "jsonClass": "MiningOperationDto",
+                                        "resources": [
+                                            {
+                                                "jsonClass": "StackDto",
+                                                "quantity": 10000,
+                                                "typeLink": "/esi/v1/universe/types/17464"
+                                            },
+                                            {
+                                                "jsonClass": "StackDto",
+                                                "quantity": 12000,
+                                                "typeLink": "/esi/v1/universe/types/1224"
+                                            }
+                                        ],
+                                        "id": "2024-02-25/30003538",
+                                        "date": "2024-02-25",
+                                        "solarSystemLink": "/esi/v1/universe/spacelocation/30003538"
                                     },
                                     {
-                                      "jsonClass": "MiningOperationDto",
-                                      "resources": [
-                                        {
-                                          "jsonClass": "StackDto",
-                                          "quantity": 210,
-                                          "typeLink": "/esi/v1/universe/types/17453"
-                                        }
-                                      ],
-                                      "id": "2024-02-23/30003541",
-                                      "date": "2024-02-23",
-                                      "solarSystemLink": "/esi/v1/universe/spacelocation/30003541"
+                                        "jsonClass": "MiningOperationDto",
+                                        "resources": [
+                                            {
+                                                "jsonClass": "StackDto",
+                                                "quantity": 210,
+                                                "typeLink": "/esi/v1/universe/types/17453"
+                                            }
+                                        ],
+                                        "id": "2024-02-23/30003541",
+                                        "date": "2024-02-23",
+                                        "solarSystemLink": "/esi/v1/universe/spacelocation/30003541"
                                     },
                                     {
-                                      "jsonClass": "MiningOperationDto",
-                                      "resources": [
-                                        {
-                                          "jsonClass": "StackDto",
-                                          "quantity": 34243,
-                                          "typeLink": "/esi/v1/universe/types/17464"
-                                        },
-                                        {
-                                          "jsonClass": "StackDto",
-                                          "quantity": 3073,
-                                          "typeLink": "/esi/v1/universe/types/1224"
-                                        },
-                                        {
-                                          "jsonClass": "StackDto",
-                                          "quantity": 10288,
-                                          "typeLink": "/esi/v1/universe/types/17459"
-                                        }
-                                      ],
-                                      "id": "2024-02-23/30003538",
-                                      "date": "2024-02-23",
-                                      "solarSystemLink": "/esi/v1/universe/spacelocation/30003538"
+                                        "jsonClass": "MiningOperationDto",
+                                        "resources": [
+                                            {
+                                                "jsonClass": "StackDto",
+                                                "quantity": 34243,
+                                                "typeLink": "/esi/v1/universe/types/17464"
+                                            },
+                                            {
+                                                "jsonClass": "StackDto",
+                                                "quantity": 3073,
+                                                "typeLink": "/esi/v1/universe/types/1224"
+                                            },
+                                            {
+                                                "jsonClass": "StackDto",
+                                                "quantity": 10288,
+                                                "typeLink": "/esi/v1/universe/types/17459"
+                                            }
+                                        ],
+                                        "id": "2024-02-23/30003538",
+                                        "date": "2024-02-23",
+                                        "solarSystemLink": "/esi/v1/universe/spacelocation/30003538"
                                     }
-                                  ])
+                                ])
                                 observer.complete();
                             })
                         }
@@ -108,7 +108,7 @@ describe('ADAPTER V1MiningOperationsAdapterService [Module: Infrastructure]', ()
             expect(service).toBeTruthy()
         })
     })
-    fdescribe('Service Delivery phase', () => {
+    describe('Service Delivery phase', () => {
         it('when the downloadMiningOperationsForCharacter is called we return a valid Promise', async () => {
             expect(adapter).toBeDefined()
             const pilotId: number = 93813310
