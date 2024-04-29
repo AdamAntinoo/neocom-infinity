1678620e32621a4d5d279ce6fffa62f20a97817d
Adam Antinooº <adamantinoo.git@gmail.com>
2024-04-29 13:11:37 +0200

    -[FIX] Fix the url for the mining operations request.

--- neocom-infinity-frontend/src/infrastructure/adapters/outbound/configuration/GlobalConstants.ts
+++ neocom-infinity-frontend/src/infrastructure/adapters/outbound/configuration/GlobalConstants.ts
@@ -1,4 +1,4 @@
 export const BACKEND_LINKS = Object.freeze({
-    BACKEND_MINING_OPERATIONS_PREFIX: '/nin/v1/character/miningoperation',
+    BACKEND_MINING_OPERATIONS_PREFIX: '/nin/v1/character/miningoperations',
     BACKEND_MINING_OPERATIONS_OPERATION:'/miningoperation'
 })
