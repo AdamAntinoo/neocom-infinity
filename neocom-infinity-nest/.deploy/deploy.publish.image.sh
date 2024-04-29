84d58fe31eb0faaa68a773db60e5f36832276a26
adam antinoo <adamantinoo.git@gmail.com>
2024-04-29 19:42:01 +0200

    -[PROJECT] Remove unused ApiSimulator binary code. -[PROJECT] Remove unwanted scripts and code.

--- neocom-infinity-nest/.deploy/deploy.publish.image.sh
+++
@@ -1,7 +0,0 @@
-# P U B L I S H    I M A G E
-# Publish the image to the external repository
-# - tag the image
-echo ">>> Tagging image->adamantinoo/%_NEOCOM.TAG%"
-docker tag %_NEOCOM.IMAGE_NAME% adamantinoo/%_NEOCOM.TAG%
-docker tag %_NEOCOM.IMAGE_NAME% %_NEOCOM.TAG%
-docker push adamantinoo/%_NEOCOM.TAG%
