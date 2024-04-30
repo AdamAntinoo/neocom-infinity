267a5d5bcb5f917a213a66656f12ac7ad4776639
adam antinoo <adamantinoo.git@gmail.com>
2024-04-30 12:15:18 +0200

    -[FIX] Fix the header construction. Headers is a singleton. -[FIX] Fix banner name and location. -[FIX] Copy all environment definitions to image. -[FIX] Update the external URLs to get data on NIN.

--- neocom-infinity-nest/src/infrastructure/adapter/outbound/ESISecureDataServices/MiningOperations/MiningOperations.typedrequest.ts
+++ neocom-infinity-nest/src/infrastructure/adapter/outbound/ESISecureDataServices/MiningOperations/MiningOperations.typedrequest.ts
@@ -3,38 +3,39 @@ import { AxiosHeaders, AxiosRequestConfig } from 'axios'
 import { TypedRequest } from 'neocom-domain'
 
 export interface MiningOperationsTypedRequestParameters {
-	characterId
-	token
+    characterId
+    token
 }
 export class MiningOperationsTypedRequest implements TypedRequest {
-	public method: string = 'GET'
-	public requestAge: number = 600
-	public request: string
-	public options: AxiosRequestConfig
+    public method: string = 'GET'
+    public requestAge: number = 600
+    public request: string
+    public options: AxiosRequestConfig
 
-	constructor(
-		private readonly parameters: MiningOperationsTypedRequestParameters,
-		private readonly configuration: ConfigService,
-	) {
-		this.method = 'GET'
-		this.prepare(parameters)
-	}
-	public prepare(parameters: MiningOperationsTypedRequestParameters): TypedRequest {
-		this.request = this.configuration.get<string>('ESI_BACKEND_HOST') + '/characters/' + parameters.characterId + '/mining/'
-		console.log('miningOperations>request->' + this.request)
-		this.options = this.prepareOptions()
+    constructor(
+        private readonly parameters: MiningOperationsTypedRequestParameters,
+        private readonly configuration: ConfigService,
+    ) {
+        this.method = 'GET'
+        this.prepare(parameters)
+    }
+    public prepare(parameters: MiningOperationsTypedRequestParameters): TypedRequest {
+        this.request = this.configuration.get<string>('ESI_BACKEND_HOST') + '/characters/' + parameters.characterId + '/mining/'
+        console.log('miningOperations>request->' + this.request)
+        this.options = this.prepareOptions()
 
-		return this
-	}
-	public prepareOptions(): AxiosRequestConfig {
-		const headers: AxiosHeaders = new AxiosHeaders()
-		headers.set('Content-Type', 'application/json')
-		headers.set('x-neocom-check', 'check header')
-		headers.set('authorization', 'Bearer ' + this.parameters.token)
-		headers.set('Cache-Control', 'max-age: ' + this.requestAge + ', public')
-		const config: AxiosRequestConfig = {
-			headers: headers,
-		}
-		return config
-	}
+        return this
+    }
+    public prepareOptions(): AxiosRequestConfig {
+        let headers: AxiosHeaders = new AxiosHeaders()
+        // WARNING - Headers is a singleton object that needs to be recreated for every set.
+        headers = headers.set('Content-Type', 'application/json')
+        headers = headers.set('x-neocom-check', 'check header')
+        headers = headers.set('authorization', 'Bearer ' + this.parameters.token)
+        headers = headers.set('Cache-Control', 'max-age: ' + this.requestAge + ', public')
+        const config: AxiosRequestConfig = {
+            headers: headers,
+        }
+        return config
+    }
 }
