2d64f1e7a81554abb27195469c283c01f11d2286
adam antinoo <adamantinoo.git@gmail.com>
2024-05-23 18:47:46 +0200

    -[API] Update the published classes generated from api.

--- neocom-openapi/generated-ts/api/api.ts
+++
@@ -1,7 +0,0 @@
-export * from './authorization.service';
-import { AuthorizationService } from './authorization.service';
-export * from './authorization.serviceInterface'
-export * from './character.service';
-import { CharacterService } from './character.service';
-export * from './character.serviceInterface'
-export const APIS = [AuthorizationService, CharacterService];
