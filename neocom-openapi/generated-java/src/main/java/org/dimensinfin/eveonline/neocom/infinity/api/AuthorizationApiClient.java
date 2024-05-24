40b3a3b26bb112c9955ef26019d70b1b08221025
adam antinoo <adamantinoo.git@gmail.com>
2024-05-24 09:05:24 +0200

    -[API] Remove files that should not be generated.

--- neocom-openapi/generated-java/src/main/java/org/dimensinfin/eveonline/neocom/infinity/api/AuthorizationApiClient.java
+++
@@ -1,8 +0,0 @@
-package org.dimensinfin.eveonline.neocom.infinity.api;
-
-import org.springframework.cloud.openfeign.FeignClient;
-import org.openapitools.configuration.ClientConfiguration;
-
-@FeignClient(name="${authorization.name:authorization}", url="${authorization.url:http://localhost}", configuration = ClientConfiguration.class)
-public interface AuthorizationApiClient extends AuthorizationApi {
-}
--- neocom-openapi/generated-java/src/main/java/org/dimensinfin/eveonline/neocom/infinity/api/CharacterApiClient.java
+++
@@ -1,8 +0,0 @@
-package org.dimensinfin.eveonline.neocom.infinity.api;
-
-import org.springframework.cloud.openfeign.FeignClient;
-import org.openapitools.configuration.ClientConfiguration;
-
-@FeignClient(name="${character.name:character}", url="${character.url:http://localhost}", configuration = ClientConfiguration.class)
-public interface CharacterApiClient extends CharacterApi {
-}
