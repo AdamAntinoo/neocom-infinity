d06d483cbc004fd29dc4bebb5e16dabc66d3f9b0
adam antinoo <adamantinoo.git@gmail.com>
2024-06-05 08:37:58 +0200

    -[CHORE] Rename the set of constants.

--- neocom-infinity-backend/src/main/java/org/dimensinfin/eveonline/neocom/infinity/infrastructure/config/GlobalAppConstants.java
+++ neocom-infinity-backend/src/main/java/org/dimensinfin/eveonline/neocom/infinity/infrastructure/config/GlobalAppConstants.java
@@ -1,7 +1,7 @@
 package org.dimensinfin.eveonline.neocom.infinity.infrastructure.config;
 
 public class GlobalAppConstants {
-	public static class TransportConstants {
+	public static class NetworkConstants {
 		public static final String NEOCOM_COOKIE_NAME = "NEOCOM-INFINITY";
 		public static final String ESI_COOKIE_NAME = "ESI-DATA-SERVICES";
 
