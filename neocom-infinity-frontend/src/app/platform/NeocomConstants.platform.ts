981e8a407b0386c870d439339965873405f50729
adam antinoo <adamantinoo.git@gmail.com>
2024-07-08 14:33:42 +0200

    -[CHORE] The state to be validated should be obtained from the configuration.

--- neocom-infinity-frontend/src/app/platform/NeocomConstants.platform.ts
+++ neocom-infinity-frontend/src/app/platform/NeocomConstants.platform.ts
@@ -1,6 +1,5 @@
 export const NeoComConstants = {
     // - C O N S T A N T S
-    VALID_STATE: 'LU5FT0NPTS5JTkZJTklUWS1QUk9EVUNUSU9OLVZBTElEIFNUQVRFIFNUUklORy0=',
     DEFAULT_ICON_PLACEHOLDER: '/assets/media/defaulticonplaceholder.png',
     DEFAULT_AVATAR_PLACEHOLDER: '/assets/media/defaultavatarplaceholder.png',
     DEFAULT_CORPORATION_AVATAR_PLACEHOLDER: '/assets/media/defaultcorporationavatar.png',
